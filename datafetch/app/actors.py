fe3cffdf31fa078ec68afd6491e28a193981fda4
Glen Egbert <glenegbert1@gmail.com>
2026-02-18 10:29:26 -0700

    update comment in acctors to include the path var

--- datafetch/app/actors.py
+++ datafetch/app/actors.py
@@ -5,9 +5,11 @@ broker = RedisBroker(url="redis://redis:6379")
 dramatiq.set_broker(broker)
 
 # This is currently being queued via a local cron which you can add like this in macos:
-
-# printf '*/5 * * * * cd /Users/glenegbert/Projects/cleancharge && /usr/local/bin/docker-compose exec -T datafetchTH=/usr/local/bin:/usr/bin:/bin\n*/5 * * * * cd /Users/glenegbert/Projects/cleancharge && docker-compose exec -T datafetch python -c "from actors import run_forecast; run_forecast.send()"\n' > /tmp/my_crontab
+# echo 'PATH=/usr/local/bin:/usr/bin:/bin' > /tmp/my_crontab
+# echo '*/5 * * * * cd /Users/glenegbert/Projects/cleancharge && docker-compose exec -T datafetch python -c
+# "from actors import run_forecast; run_forecast.send()"' >> /tmp/my_crontab
 # crontab /tmp/my_crontab
+
 
 @dramatiq.actor
 def run_forecast():
